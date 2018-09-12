c14dc6fb0ac9f3ae40046a022174376684be787b
huynhtnhut <luminous997@gmail.com>
2018-09-12 17:03:04 +0700

    update file

---
+++ ProjectManager/DTO/Sprint.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DTO;
+namespace DTO
+{
+    class Sprint
+    {
+        int projectID;
+        int order;
+        DateTime startDate;
+        DateTime endDate;
+        SprintState sprintState;
+        string description;
+    }
+}
---
+++ ProjectManager/DTO/UserStory.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DTO;
+namespace DTO
+{
+    class UserStory
+    {
+        //List<string> userStoryState = new List<string>();
+        int id;
+        string title;
+        string description;
+        int projectID;
+        User assigneeUser;
+        UserStoryState userStoryState;
+    }
+}
